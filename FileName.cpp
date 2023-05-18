ee4d944af6b94f6e90bfd121ae64dd8076162575
林岚 <72348612+zenghao-1@users.noreply.github.com>
2023-05-18 23:58:33 +0800

    Add files via upload

---
+++ FileName.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string He = "Hello";
+	string D = ",";
+	string Wor = "wordl";
+	string G = "!";
+
+	cout << He << endl << D << endl << Wor << endl << G << endl;
+	cout << He + D + Wor + G << endl;
+}
